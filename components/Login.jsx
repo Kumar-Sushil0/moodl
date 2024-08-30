2bf13f6e9b72fad89e672f7e6fab222beb195849
Kumar-Sushil0 <skorpion334450@gmail.com>
2024-08-30 16:35:28 +0530

    Fixed a minor issue

--- components/Login.jsx
+++ components/Login.jsx
@@ -4,58 +4,69 @@ import { Fugaz_One } from 'next/font/google';
 import Button from './Button';
 import React, { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
-const fugaz = Fugaz_One({ subsets: ["latin"],weight:["400"] });
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
+
 function Login() {
-  const[email,setEmail]=useState('');
-  const[password,setPassword]=useState('')
-  const[isRegister,setIsRegister]=useState(false);
-  const[authenticating,setAuthenticating]=useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegister, setIsRegister] = useState(false);
+  const [authenticating, setAuthenticating] = useState(false);
 
-  const{signup,login}=useAuth();
-
+  const { signup, login } = useAuth();
 
   async function handleSubmit() {
-    if(password.length<6){
-      alert("Password should not be less than 6 characters/digits")
-     
+    if (password.length < 6) {
+      alert("Password should not be less than 6 characters/digits");
     }
-    if(!email || !password ||password.length<6){
-      return
+    if (!email || !password || password.length < 6) {
+      return;
     }
-    
+
     setAuthenticating(true);
 
-    try{
-      if(isRegister){
+    try {
+      if (isRegister) {
         console.log("Signing up a new user");
-        await signup(email,password);
-      }else{
+        await signup(email, password);
+      } else {
         console.log("Logging in existing user");
-        await login(email,password)
+        await login(email, password);
       }
-      
-    }catch(err){
-      console.log(err.message)
-    }finally{
+    } catch (err) {
+      console.log(err.message);
+    } finally {
       setAuthenticating(false);
     }
-    
   }
-
-
-
 
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
-      <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>{isRegister? 'Register':'Login'}</h3>
+      <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>{isRegister ? 'Register' : 'Login'}</h3>
       <p>You're one step away!</p>
-      <input value={email} onChange={(e)=>{setEmail(e.target.value)}} className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none" placeholder='Email'/>
-      <input value={password} onChange={(e)=>{setPassword(e.target.value)}} className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none" placeholder='Password' type='password'/>
-        <div className="max-w-[400px] w-full mx-auto">
-        <Button clickHandler={handleSubmit} text={authenticating?"Submitting...":"Submit"} full/>
-        </div>
-      <p className='text-center'>{isRegister?"Already have an account?" :"Don't have an account?" } <span onClick={()=>{setIsRegister(!isRegister)}} className='text-indigo-600 cursor-pointer'>{isRegister?'Sign in': 'Sign up'}</span></p>
+      <input
+        value={email}
+        onChange={(e) => { setEmail(e.target.value) }}
+        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none"
+        placeholder='Email'
+      />
+      <input
+        value={password}
+        onChange={(e) => { setPassword(e.target.value) }}
+        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none"
+        placeholder='Password'
+        type='password'
+      />
+      <div className="max-w-[400px] w-full mx-auto">
+        <Button clickHandler={handleSubmit} text={authenticating ? "Submitting..." : "Submit"} full />
       </div>
+      <p className='text-center'>
+        {isRegister ? "Already have an account?" : "Don&apos;t have an account?"}
+        <span onClick={() => { setIsRegister(!isRegister) }} className='text-indigo-600 cursor-pointer'>
+          {isRegister ? 'Sign in' : 'Sign up'}
+        </span>
+      </p>
+    </div>
   );
 }
 
