ebe22aaf889f4da9d954a93266897f877d03eac1
Kumar-Sushil0 <skorpion334450@gmail.com>
2024-08-30 16:53:19 +0530

    Fixed another minor issue

--- components/Login.jsx
+++ components/Login.jsx
@@ -42,27 +42,31 @@ function Login() {
 
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
-      <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>{isRegister ? 'Register' : 'Login'}</h3>
+      <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>
+        {isRegister ? 'Register' : 'Login'}
+      </h3>
       <p>You're one step away!</p>
-      <input
-        value={email}
-        onChange={(e) => { setEmail(e.target.value) }}
-        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none"
-        placeholder='Email'
+      <input 
+        value={email} 
+        onChange={(e) => { setEmail(e.target.value); }} 
+        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none" 
+        placeholder='Email' 
       />
-      <input
-        value={password}
-        onChange={(e) => { setPassword(e.target.value) }}
-        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none"
-        placeholder='Password'
-        type='password'
+      <input 
+        value={password} 
+        onChange={(e) => { setPassword(e.target.value); }} 
+        className="max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border-2 border-solid border-indigo-400 rounded-full outline-none" 
+        placeholder='Password' 
+        type='password' 
       />
       <div className="max-w-[400px] w-full mx-auto">
         <Button clickHandler={handleSubmit} text={authenticating ? "Submitting..." : "Submit"} full />
       </div>
       <p className='text-center'>
         {isRegister ? "Already have an account?" : "Don&apos;t have an account?"}
-        <span onClick={() => { setIsRegister(!isRegister) }} className='text-indigo-600 cursor-pointer'>
+        <span 
+          onClick={() => { setIsRegister(!isRegister); }} 
+          className='text-indigo-600 cursor-pointer'>
           {isRegister ? 'Sign in' : 'Sign up'}
         </span>
       </p>
